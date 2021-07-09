724092193add11bd2f67bd23b1ca7a27a13ec13e
o0Manavaul0o <87138886+o0Manavaul0o@users.noreply.github.com>
2021-07-09 01:50:21 -0500

    Initial commit

---
+++ Lab - Constants and Variables.playground/Pages/3. Exercise - Variables.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+/*:
+ ## Exercise - Variables
+ 
+ Declare a variable `schooling` and set it to the number of years of school that you have completed. Print `schooling` to the console.
+ */
+var schooling = 10
+
+/*:
+ Now imagine you just completed an additional year of school, and update the `schooling` variable accordingly. Print `schooling` to the console.
+ */
+schooling = 11
+print(schooling)
+/*:
+ Does the above code compile? Why is this different than trying to update a constant? Print your explanation to the console using the `print` function.
+ */
+print("The code compiles.")
+print("Declaring 'scholing' as a var makes it changable.")
+
+//: [Previous](@previous)  |  page 3 of 10  |  [Next: App Exercise - Step Count](@next)
---
+++ Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+/*:
+ ## Exercise - Types and Type Safety
+ 
+ Declare two variables, one called `firstDecimal` and one called `secondDecimal`. Both should have decimal values. Look at both of their types by holding Option and clicking on the variable name.
+ */
+var firstDecimal = 1.1
+var secondDecimal = 1.2
+/*:
+ Declare a variable called `trueOrFalse` and give it a boolean value. Try to assign it to `firstDecimal` like so: `firstDecimal = trueOrFalse`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
+ */
+var trueOrFalse = true
+print("Will not compile; cannot assign boolean to double.")
+/*:
+ Declare a variable and give it a string value. Then try to assign it to `firstDecimal`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
+ */
+var string = "my string here"
+print("Does not compile, cannot assign type double to string.")
+/*:
+ Finally, declare a variable with a whole number value. Then try to assign it to `firstDecimal`. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
+ */
+var whole = 1
+print("Does not compile; cannot assign type int to double.")
+//: [Previous](@previous)  |  page 7 of 10  |  [Next: App Exercise - Tracking Different Types](@next)
